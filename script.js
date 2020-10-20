a7f2b200a9212544f7489a993db934de914dab24
Joseph <jb134@hw.ac.uk>
2020-10-20 14:55:07 +0100

    movePlayer implemented

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 var block =  document.getElementById("block");
+var gameBox = document.getElementById("gamebox");
+var character = document.getElementById("character");
 
-//let iterationCount = 0;
 block.addEventListener('animationiteration', () => {
-   // iterationCount++;
     var random = (Math.random() * 800);
     block.style.left = random +"px";
 });
+
+var move = 0;
+
+
+
+function movePlayer(e) {
+    if(e.keyCode == 37) {
+        move -= 20;
+        character.style.left = move + "px"; //move left
+    }
+    else if(e.keyCode == 39) {
+        move += 20;
+        character.style.left = move + "px"; //move right
+    }
+};
+
+document.onkeydown =movePlayer;
