494b514e6e685013b2c71a297301088f1a2cc861
mub27 <mubeen_2001@live.co.uk>
2020-11-12 11:16:05 +0000

    added socket.emit for showing players moving

--- Web-Programming/Client/character.js
+++ Web-Programming/Client/character.js
@@ -2,14 +2,18 @@ var character =  document.getElementById("character");
 
 var move = 0;
 
+var socket = io();
+
 function movePlayer(e) {
     if(e.keyCode == 37) {
         move -= 20;
         character.style.left = move + "px"; //move left
+        //socket.emit('keyPress',{inputId:'left',state:true});
     }
     else if(e.keyCode == 39) {
         move += 20;
         character.style.left = move + "px"; //move right
+        //socket.emit('keyPress',{inputId:'right',state:true});
     }
 };
 
