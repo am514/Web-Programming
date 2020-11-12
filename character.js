d34a1a31566647418a22a4bf3ca73639f615b850
Joseph <jb134@hw.ac.uk>
2020-11-12 19:03:26 +0000

    create each block attribute in javascript

--- character.js
+++ character.js
@@ -1,16 +1,48 @@
 let character =  document.getElementById("character");
 
-let move = 0;
+let interval;
+let both = 0;
 
-function movePlayer(e) {
-    if(e.keyCode == 37) {
-        move -= 20;
-        character.style.left = move + "px"; //move left
-    }
-    else if(e.keyCode == 39) {
-        move += 20;
-        character.style.left = move + "px"; //move right
-    }
-};
 
-document.onkeydown =movePlayer;
+
+
+
+
+
+
+function moveLeft(){
+
+    let left = parseInt(window.getComputedStyle(character).getPropertyValue("left"));
+    if(left > 0 ){
+        character.style.left = left - 2 + "px";
+    }
+    
+}
+
+function moveRight(){
+    let left = parseInt(window.getComputedStyle(character).getPropertyValue("left"));
+    if (left < 760 ){
+        character.style.left = left + 2 + "px";
+    }
+    
+}
+
+
+document.addEventListener("keydown", event => {
+    if(both == 0){
+        both++;
+        if(event.key === "ArrowLeft"){
+            interval= setInterval(moveLeft, 1);
+
+        }
+        if(event.key === "ArrowRight"){
+            interval= setInterval(moveRight, 1);
+        
+        }
+    }
+});
+
+document.addEventListener("keyup", event =>{
+    clearInterval(interval);
+    both = 0;
+});
--- fallingBlock.js
+++ fallingBlock.js
@@ -1,8 +1,20 @@
-let block =  document.getElementById("block");
+
 let gameBox = document.getElementById("gamebox");
+let counter = 0;
 
 
+// block.addEventListener('animationiteration', () => {
+//     let random = (Math.random() * 800);
+//     block.style.left = random +"px";
+// });
+
+
+let block = document.createElement("div");
+block.setAttribute("id", "block");
 block.addEventListener('animationiteration', () => {
     let random = (Math.random() * 800);
     block.style.left = random +"px";
 });
+gameBox.appendChild(block);
+
+
