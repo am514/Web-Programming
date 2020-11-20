bb050ea3d4f4212af37f525a699207dabd5b354a
Joseph <jb134@hw.ac.uk>
2020-11-20 06:39:47 +0000

    balls have been added x

--- Web-Programming/Client/character.js
+++ Web-Programming/Client/character.js
@@ -1,3 +1,27 @@
+// var character =  document.getElementById("character");
+
+// var move = 0;
+
+// var socket = io();
+
+// function movePlayer(e) {
+//     if(e.keyCode == 37) {
+//         move -= 20;
+//         character.style.left = move + "px"; //move left
+//         //socket.emit('keyPress',{inputId:'left',state:true});
+//     }
+//     else if(e.keyCode == 39) {
+//         move += 20;
+//         character.style.left = move + "px"; //move right
+//         //socket.emit('keyPress',{inputId:'right',state:true});
+//     }
+// };
+
+// document.onkeydown =movePlayer;
+
+
+
+
 let gameBox = document.getElementById("gamebox"); //define fame box, area where game will take place
 
 
