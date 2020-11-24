1116254dba3443b0bec685a658dd3c83b4fcb65e
mub27 <mubeen_2001@live.co.uk>
2020-11-24 12:16:40 +0000

    added socket code to communicate with the server

--- Web-Programming-0.1/Server0.1/Client/character.js
+++ Web-Programming-0.1/Server0.1/Client/character.js
@@ -24,7 +24,7 @@
 
 let gameBox = document.getElementById("gamebox"); //define fame box, area where game will take place
 
-
+var socket = io(); // connects the player to the server
 
 class Character {
     constructor(y,width,height,color) { // character attributes as constructor parameter
@@ -35,33 +35,33 @@ class Character {
         this.height = height; //sets height to the value passed in the parameter
         this.color = color; //sets color to the value passed in the parameter
         this.sketched = false; //whether the character has been spawned in yet
-        this.velocity = 10; //starting velocity is zero while the 
+        this.velocity = 10; //starting velocity is zero while the
         this.acceleration = 1.25; // acceleration is set to 0.5
         this.movingLeft = false; //moving left set to false
         this.movingRight = false; //moving right set to false
     }
 
     sketchCharacter(){
-        
+
         if (!this.sketched){ //if the character has not been spawned in yet then the code below is executed
             this.sketched = true; // sets sketched to true
-            
+
             this.character.setAttribute("id", "character"); // sets the div to ID
             this.character.style.left = this.x + "px"; // sets the left position, x
             this.character.style.top = this.y + "px";  // sets the left position, y
             this.character.style.width = this.width + "px"; // sets the width
             this.character.style.height = this.height + "px"; // sets the width
             this.character.style.background = this.color; // sets the color of the character
-            this.character.style.position = "relative"; 
+            this.character.style.position = "relative";
             this.character.style.zIndex = 100;
             gameBox.appendChild(this.character); //places the new character element inside of the game box
         }
         else{ //if the character has already been spawned in
 
-            
+
             if(this.movingLeft || this.movingRight){ //if the character is moving the code below is ran
                 if(this.x < 0){ //character pops out on the opposite sides if it ever reaches the sides of the game box
-                    this.x = 798; 
+                    this.x = 798;
                     this.character.style.left = this.x + "px";
 
                 }
@@ -76,13 +76,13 @@ class Character {
                     this.velocity *= this.acceleration;  //increases the character velocity by the value of the acceleration
                     this.character.style.left = this.x + "px"; //
                 }
-                
+
             }
             else if(!(this.movingLeft) && !(this.movingRight)){ //if the character is still the velocity will gradually decrease
                 this.character.style.left = this.x + "px";
             }
-            
-            
+
+
         }
     }
     moveLeft(){ //move left function, sets movingLeft to true and decreases the value of x
@@ -119,8 +119,8 @@ class Character {
 
         }
     }
-    
-    
+
+
 
 }
 
@@ -136,9 +136,11 @@ function keyDown(event){
     event = event || window.event;
     if (event.keyCode == "37") {
         newCharacter.moveLeft();
+        socket.emit('keyPress',{inputId:'left',state:true});
     }
     else if(event.keyCode == "39"){
         newCharacter.moveRight();
+        socket.emit('keyPress',{inputId:'right',state:true});
     }
 }
 
@@ -146,5 +148,5 @@ document.onkeyup = keyUp;
 function keyUp(event){
     event = event || window.event;
     newCharacter.reduceVelocity();
-    
+
 }
