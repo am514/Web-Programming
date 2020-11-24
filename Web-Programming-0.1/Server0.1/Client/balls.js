925d6769cb0bc6966f76044bcf0cf810ab4c5156
Roger-Design <57538941+Roger-Design@users.noreply.github.com>
2020-11-24 11:34:28 +0000

    Client update
    Client code update with changes to browser pages and the addition of JB code to allow for collision and score updates.

---
+++ Web-Programming-0.1/Server0.1/Client/balls.js
@@ -0,0 +1,76 @@
+let counter = 0;
+let score = 0;
+let currentBlocks = [];
+// create falling block and add it to html
+setInterval(function(){ 
+
+    let blockLast = document.getElementById("block" + (counter -1));
+
+    if(counter>0){
+        var blockLastTop = parseInt(window.getComputedStyle(blockLast).getPropertyValue("top"));
+    }
+
+     if((blockLastTop < 800 || counter == 0 ) && counter < 6 ){
+        let block = document.createElement("div");
+        block.setAttribute("class", "block");
+        block.style.left = (Math.random() * 760) + "px";
+        block.setAttribute("id", "block" + counter);
+        //     let random = (Math.random() * 760);
+        block.style.top = (blockLastTop + 200) + "px";
+        //     block.style.left = random +"px";
+        gameBox.appendChild(block);
+        // if(block.style.top > 0 && block.style.top < 760 ){
+            currentBlocks.push(counter);
+        //}
+
+
+        counter++;
+    }
+
+    for(var i = 0; i < currentBlocks.length; i++){
+        let collides = false;
+        let current =currentBlocks[i];
+        let indexblock = document.getElementById("block" + current);
+        let blockTop = parseFloat(window.getComputedStyle(indexblock).getPropertyValue("top"));
+        let blockBot = (blockTop + parseFloat(window.getComputedStyle(indexblock).getPropertyValue("height")));
+        let blockLeft = parseFloat(window.getComputedStyle(indexblock).getPropertyValue("left"));
+        let blockRight = (blockLeft + parseFloat(window.getComputedStyle(indexblock).getPropertyValue("width")));
+        indexblock.style.top = blockTop + 0.5 + "px";
+        if(blockTop == 1200){
+            indexblock.style.top = 0 + "px";
+            indexblock.style.left = (Math.random() * 760) + "px";
+        }
+        if(!(  
+            ( (newCharacter.y + newCharacter.height) < blockTop)
+            || (newCharacter.y > blockBot) 
+            || ((newCharacter.x + newCharacter.width) < blockLeft) 
+            || (newCharacter.x > blockRight)
+            
+            )){
+                indexblock.style.top = -100 + "px";
+                indexblock.style.left = (Math.random() * 760) + "px";
+                collides = true;
+                score ++;
+                console.log("score: " + score);
+
+                
+            }
+
+        
+    }
+
+
+
+},1);
+
+
+
+
+
+
+
+//scoreboard for player
+
+
+//create score board 
+
--- Web-Programming-0.1/Server0.1/Client/character.js
+++ Web-Programming-0.1/Server0.1/Client/character.js
@@ -1,59 +1,150 @@
-let character =  document.getElementById("character");
+// var character =  document.getElementById("character");
 
-let interval;
-let both = 0;
+// var move = 0;
+
+// var socket = io();
+
+// function movePlayer(e) {
+//     if(e.keyCode == 37) {
+//         move -= 20;
+//         character.style.left = move + "px"; //move left
+//         //socket.emit('keyPress',{inputId:'left',state:true});
+//     }
+//     else if(e.keyCode == 39) {
+//         move += 20;
+//         character.style.left = move + "px"; //move right
+//         //socket.emit('keyPress',{inputId:'right',state:true});
+//     }
+// };
+
+// document.onkeydown =movePlayer;
 
 
 
 
+let gameBox = document.getElementById("gamebox"); //define fame box, area where game will take place
 
 
 
-
-function moveLeft(){
-    
-    let left = parseInt(window.getComputedStyle(character).getPropertyValue("left"));
-    let speed = 2;
-    if(left > -40 ){
-        character.style.left = left - speed + "px";
-        //socket.emit('keyPress',{inputId:'left',state:true});
+class Character {
+    constructor(y,width,height,color) { // character attributes as constructor parameter
+        this.character = document.createElement("div");
+        this.x = 400; //sets x position , left in css, to 400, half the game box
+        this.y = y; //sets y position , top in css, to the value passed in the parameter when new Character is created
+        this.width = width;  //sets width to the value passed in the parameter
+        this.height = height; //sets height to the value passed in the parameter
+        this.color = color; //sets color to the value passed in the parameter
+        this.sketched = false; //whether the character has been spawned in yet
+        this.velocity = 10; //starting velocity is zero while the 
+        this.acceleration = 1.25; // acceleration is set to 0.5
+        this.movingLeft = false; //moving left set to false
+        this.movingRight = false; //moving right set to false
     }
-    else{
-        character.style.left = 800 + "px";
-    }
-    
-    
-}
 
-function moveRight(){
-    let speed = 2;
-    let left = parseInt(window.getComputedStyle(character).getPropertyValue("left"));
-    if (left < 800 ){
-        character.style.left = left + speed + "px";
-        //socket.emit('keyPress',{inputId:'right',state:true});
-    }
-    else{
-        character.style.left = 0 + "px";
-    }
-    
-}
-
-
-document.addEventListener("keydown", event => {
-    if(both == 0){
-        both++;
-        if(event.key === "ArrowLeft"){
-            interval = setInterval(moveLeft, 1);
-
-        }
-        if(event.key === "ArrowRight"){
-            interval = setInterval(moveRight, 1);
+    sketchCharacter(){
         
+        if (!this.sketched){ //if the character has not been spawned in yet then the code below is executed
+            this.sketched = true; // sets sketched to true
+            
+            this.character.setAttribute("id", "character"); // sets the div to ID
+            this.character.style.left = this.x + "px"; // sets the left position, x
+            this.character.style.top = this.y + "px";  // sets the left position, y
+            this.character.style.width = this.width + "px"; // sets the width
+            this.character.style.height = this.height + "px"; // sets the width
+            this.character.style.background = this.color; // sets the color of the character
+            this.character.style.position = "relative"; 
+            this.character.style.zIndex = 100;
+            gameBox.appendChild(this.character); //places the new character element inside of the game box
+        }
+        else{ //if the character has already been spawned in
+
+            
+            if(this.movingLeft || this.movingRight){ //if the character is moving the code below is ran
+                if(this.x < 0){ //character pops out on the opposite sides if it ever reaches the sides of the game box
+                    this.x = 798; 
+                    this.character.style.left = this.x + "px";
+
+                }
+                else if(this.x > 800){ //for right side
+                    this.x = 0;
+                    this.character.style.left = this.x + "px";
+                }
+                else if(this.velocity >= 30){ //if the velocity reaches 20 which we will make the maximum, it does not increase anymore
+                    this.character.style.left = this.x + "px";
+                }
+                else{
+                    this.velocity *= this.acceleration;  //increases the character velocity by the value of the acceleration
+                    this.character.style.left = this.x + "px"; //
+                }
+                
+            }
+            else if(!(this.movingLeft) && !(this.movingRight)){ //if the character is still the velocity will gradually decrease
+                this.character.style.left = this.x + "px";
+            }
+            
+            
         }
     }
-});
+    moveLeft(){ //move left function, sets movingLeft to true and decreases the value of x
+        this.movingLeft = true;
+        this.movingRight = false;
+        // console.log(this.velocity);
+        this.x -= this.velocity;
+        this.sketchCharacter();
 
-document.addEventListener("keyup", event =>{
-    clearInterval(interval);
-    both = 0;
-});
+    }
+    moveRight(){ //move left function, sets movingRight to true and increases the value of x
+        this.movingLeft = false;
+        this.movingRight = true;
+        // console.log(this.velocity);
+        this.x += this.velocity;
+        this.sketchCharacter();
+    }
+
+    //////////////////////////////////////
+    //////code below tries but fails//////
+    /////to implement decceleration///////
+    ////////////////instead //////////////
+    //////////////////////////////////////
+    //////////////////////////////////////
+
+    reduceVelocity(){
+        if(!this.movingLeft){
+            this.velocity = 10;
+            this.sketchCharacter();
+        }
+        else if(!this.movingRight){
+            this.velocity = 10;
+            this.sketchCharacter();
+
+        }
+    }
+    
+    
+
+}
+
+
+
+
+
+
+let newCharacter = new Character(778,50,20,"green");
+newCharacter.sketchCharacter();
+document.onkeydown = keyDown;
+function keyDown(event){
+    event = event || window.event;
+    if (event.keyCode == "37") {
+        newCharacter.moveLeft();
+    }
+    else if(event.keyCode == "39"){
+        newCharacter.moveRight();
+    }
+}
+
+document.onkeyup = keyUp;
+function keyUp(event){
+    event = event || window.event;
+    newCharacter.reduceVelocity();
+    
+}
