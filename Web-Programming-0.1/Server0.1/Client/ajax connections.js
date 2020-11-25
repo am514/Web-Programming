e3a5fc6e20038ddf10b9fe93dc1e95135ff926ee
am514 <71259868+am514@users.noreply.github.com>
2020-11-25 12:55:07 +0000

    ajax get request

---
+++ Web-Programming-0.1/Server0.1/Client/ajax connections.js
@@ -0,0 +1,13 @@
+//sets up a ajax connection get request
+
+//creates the http request
+var xhr = new XMLHttpRequest();
+
+//call back function
+xhr.onreadystatechange = function() {};
+
+//gets request from the specified domain
+xhr.open('GET', '');//in the empty speech marks put the html page from the server side
+
+//sends the request
+xhr.send()
